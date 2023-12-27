5c411ebcf01f7d48a8973d3730f1ee084412a933
Alfonso Patron <c9alfonsopatron@hotmail.com>
2023-12-27 15:20:58 -0700

    Initial Commit

---
+++ app/src/main/java/com/patron/masacoporal/HomeViewActyivity.kt
@@ -0,0 +1,11 @@
+package com.patron.masacoporal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeViewActyivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_view_actyivity)
+    }
+}
