2ea4e323f9ff07df9bd7516ca9f8e433784f1e91
Alfonso Patron <c9alfonsopatron@hotmail.com>
2023-12-28 21:11:15 -0700

    - Add Weight Button - Add the logic and functionality of Weight Card - Add the svg icons "Plus" and "Decrese"

--- app/src/main/java/com/patron/masacoporal/HomeViewActyivity.kt
+++ app/src/main/java/com/patron/masacoporal/HomeViewActyivity.kt
@@ -6,17 +6,23 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
 class HomeViewActyivity : AppCompatActivity() {
 
+    private var currentWeight : Int = 60
     private var isMaleSected: Boolean = true
     private var isFemaleSelected : Boolean = false
     private lateinit var cardMale: CardView
     private lateinit var cardFemale: CardView
     private lateinit var tvHeight : TextView
     private lateinit var rsHeight : RangeSlider
+    private lateinit var tvWeight : TextView
+    private lateinit var fbSubstractWeight : FloatingActionButton
+    private lateinit var fbPlusWeight : FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -47,6 +53,24 @@ class HomeViewActyivity : AppCompatActivity() {
             tvHeight.text = "$result cm"
 
         }
+
+        fbSubstractWeight.setOnClickListener{
+            if (currentWeight >= 21) {
+                currentWeight--
+                tvWeight.text = currentWeight.toString()
+            }
+
+        }
+        fbPlusWeight.setOnClickListener{
+            // Sumar a la edad
+            if (currentWeight <= 199) {
+                currentWeight++
+                tvWeight.text = currentWeight.toString()
+            }
+
+
+        }
+
     }
 
     private fun changeGender(){
@@ -59,6 +83,10 @@ class HomeViewActyivity : AppCompatActivity() {
         cardFemale = findViewById(R.id.cardFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        tvWeight.text = currentWeight.toString()
+        fbSubstractWeight = findViewById(R.id.fbSubtractWeight)
+        fbPlusWeight = findViewById(R.id.fbPlusWeight)
     }
 
     private fun setGenderColor() {
